4719d6b2ecc43947e1a914ff80fbcd4f8826b529
Mark Watts <wattsmark2015@gmail.com>
2014-11-16 20:35:03 -0600

    Added slightly fancier play status animation
    - Restructed some code

--- xmms2-string.rb
+++ xmms2-string.rb
@@ -13,6 +13,7 @@ $CONNECTED=false
 $paused = false
 $repeat = "off"
 $stopped = false
+$tiddle = 0
 
 while not $CONNECTED
     begin
@@ -46,6 +47,11 @@ def extract_medialib_info(id, *fields)
     end
     res
 end
+
+def decode_xmms2_url (url)
+    URI.decode_www_form_component(url)
+end
+
 $fields = [:title, :artist, :album]
 
 $info = nil
@@ -60,23 +66,28 @@ def get_string(sep="::",undef_string="UNDEF")
         string = info.map{|i| "#{i[0,max_width]}" }.join(sep) << "\n"
         max_width -= 1
     end while (string.real_length > 70)
-    $string = string
+    string
+end
+
+def get_repeat_status
+    pl = $xc.config_get_value('playlist.repeat_all').wait.value
+    tr = $xc.config_get_value('playlist.repeat_one').wait.value
+    if pl == "1"
+        "playlist"
+    elsif tr == "1"
+        "track"
+    else
+        "off"
+    end
 end
 
 $xc.broadcast_playback_current_id.notifier do |res|
-    get_string
+    $string = get_string
     true
 end
 
 $xc.broadcast_config_value_changed.notifier do |res|
-    if res['playlist.repeat_all'.to_sym] == "1"
-        $repeat = "playlist"
-    elsif res['playlist.repeat_one'.to_sym] == "1"
-        $repeat = "track"
-    else
-        $repeat = "off"
-    end
-    get_string
+    $repeat = get_repeat_status
     true
 end
 
@@ -90,11 +101,9 @@ $xc.broadcast_playback_status.notifier do |res|
         $stopped = true
         $paused = true
     end
-    get_string
+    update
     true
 end
-
-#get_string
 
 if not(File.exists?($PIPE_PATH) and File.pipe?($PIPE_PATH))
     begin
@@ -104,10 +113,36 @@ if not(File.exists?($PIPE_PATH) and File.pipe?($PIPE_PATH))
     `mkfifo #{$PIPE_PATH}`
 end
 
-$stdout = File.open($PIPE_PATH,"w")
-$stderr = $LOG_FILE
-$tiddle = true
-while true do
+def get_xmobar_string(tiddle, playtime, track_info_string)
+    str = "XMMS2"
+    if not($info.nil? or playtime.nil?)
+        d = $info[:duration].to_i
+        # The '- 1' is to prevent us from putting the </fc> on
+        # a new line and mucking up xmobar's line-based parse
+        r = [track_info_string.length - 1, (playtime * track_info_string.length) / d].min
+        r = [0, r].max
+        play_anim = "-\\|/"
+        play_anim_count = tiddle % play_anim.length
+        start_symbol = if $stopped
+                           "<fc=#ff0000>*</fc>"
+                       elsif $paused
+                           "*"
+                       else
+                           "<fc=#8aadb9>"+play_anim[play_anim_count] +"</fc>"
+                       end
+        repeat_symbol = if $repeat == "track"
+                            "T"
+                        elsif $repeat == "playlist"
+                            "P"
+                        else
+                            "-"
+                        end
+        str = start_symbol + repeat_symbol + " " + String.new(track_info_string).insert(r, "</fc>").insert(0,"<fc=#8aadb9>")
+    end
+    str
+end
+
+def update
     begin
         p = $xc.playback_playtime.wait.value
     rescue TypeError => e
@@ -118,29 +153,8 @@ while true do
     end
 
     begin
-        if not($info.nil? or p.nil?)
-            d = $info[:duration].to_i
-            # The '- 1' is to prevent us from putting the </fc> on
-            # a new line and mucking up xmobar's line-based parse
-            r = [$string.length - 1, (p * $string.length) / d].min
-            r = [0, r].max
-            start_symbol = if $paused
-                               "*"
-                           elsif $tiddle
-                               "/"
-                           else
-                               "\\"
-                           end
-            repeat_symbol = if $repeat == "track"
-                                "↻1"
-                            elsif $repeat == "playlist"
-                                "↻"
-                            else
-                                ""
-                            end
-
-            print start_symbol + " " + repeat_symbol + String.new($string).insert(r, "</fc>").insert(0,"<fc=#8aadb9>")
-        end
+        $stdout.puts get_xmobar_string($tiddle, p, $string)
+        $stdout.flush
     rescue Xmms::Client::ClientError => e
         $LOG_FILE.print "Server died. Dying.\n"
     rescue => e
@@ -148,7 +162,14 @@ while true do
         $LOG_FILE.puts e.backtrace
         raise e
     end
-    $stdout.flush
-    $tiddle = !$tiddle
+end
+
+$stdout = File.open($PIPE_PATH,"w")
+$stderr = $LOG_FILE
+$string = get_string
+$repeat = get_repeat_status
+while true do
+    update
+    $tiddle = $tiddle + 1
     sleep 1
 end

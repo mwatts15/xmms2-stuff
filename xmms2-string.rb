dfafda3f8a6b0c34d870a75e23f7def94b57a3e9
Mark Watts <wattsmark2015@gmail.com>
2015-02-10 12:59:56 -0600

    Added recovery from a broken pipe
    - Made the process 'daemonize' with Process.daemon

--- xmms2-string.rb
+++ xmms2-string.rb
@@ -7,12 +7,13 @@ require 'socket'
 $PIPE_PATH = "/tmp/#{ENV["USER"]}-xmms2-string-ipc-pipe"
 $LOG_PATH = "#{ENV["HOME"]}/.config/xmms2/xmms2-string.log"
 $LOG_FILE = nil
-
+$DISCONNECTED = false
 $xc = Xmms::client("xmms2-stirg")
 $LOG_FILE = File.open($LOG_PATH,"w")
 $xc.on_disconnect do
     $LOG_FILE.print "Server died. Getting the hell out of Dodge.\n"
     exit
+    #$DISCONNECTED = true
 end
 
 $paused = false
@@ -88,29 +89,31 @@ end
 
 def get_xmobar_string(tiddle, playtime, track_info_string)
     str = "XMMS2"
-    if not($info.nil? or playtime.nil?)
+    if not($info.nil? or playtime.nil? or track_info_string.length == 0)
         d = $info[:duration].to_i
         # The '- 1' is to prevent us from putting the </fc> on
         # a new line and mucking up xmobar's line-based parse
-        r = [track_info_string.length - 1, (playtime * track_info_string.length) / d].min
-        r = [0, r].max
-        play_anim = "-\\|/"
-        play_anim_count = tiddle % play_anim.length
-        start_symbol = if $stopped
-                           "<fc=#ff0000>*</fc>"
-                       elsif $paused
-                           "*"
-                       else
-                           "<fc=#8aadb9>"+play_anim[play_anim_count] +"</fc>"
-                       end
-        repeat_symbol = if $repeat == "track"
-                            "T"
-                        elsif $repeat == "playlist"
-                            "P"
-                        else
-                            "-"
-                        end
-        str = start_symbol + repeat_symbol + " " + String.new(track_info_string).insert(r, "</fc>").insert(0,"<fc=#8aadb9>")
+        if d > 0
+            r = [track_info_string.length - 1, (playtime * track_info_string.length) / d].min
+            r = [0, r].max
+            play_anim = "-\\|/"
+            play_anim_count = tiddle % play_anim.length
+            start_symbol = if $stopped
+                               "<fc=#ff0000>*</fc>"
+                           elsif $paused
+                               "*"
+                           else
+                               "<fc=#8aadb9>"+play_anim[play_anim_count] +"</fc>"
+                           end
+            repeat_symbol = if $repeat == "track"
+                                "T"
+                            elsif $repeat == "playlist"
+                                "P"
+                            else
+                                "-"
+                            end
+            str = start_symbol + repeat_symbol + " " + String.new(track_info_string).insert(r, "</fc>").insert(0,"<fc=#8aadb9>")
+        end
     end
     str
 end
@@ -129,7 +132,9 @@ def update
         $stdout.puts get_xmobar_string($tiddle, p, $string)
         $stdout.flush
     rescue Xmms::Client::ClientError => e
-        $LOG_FILE.print "Server died. Dying.\n"
+        $LOG_FILE.puts "Server died.\n"
+    rescue Errno::EPIPE => e
+        $LOG_FILE.puts "Broken pipe (#{e.inspect}): \n"
     rescue => e
         $LOG_FILE.print "Some error (#{e.inspect}): \n"
         $LOG_FILE.puts e.backtrace
@@ -142,6 +147,7 @@ $stderr = $LOG_FILE
 $string = get_string
 $repeat = get_repeat_status
 $paused, $stopped = decode_playback_status($xc.playback_status.wait.value)
+Process.daemon
 while true do
     update
     $tiddle = $tiddle + 1
